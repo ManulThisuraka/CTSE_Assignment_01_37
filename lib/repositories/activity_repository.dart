9bda6d317ad77562376574a771bccca08f614754
Chalanka <chalaji99@gmail.com>
2023-03-25 17:37:40 +0530

    Activity Repositary added

--- lib/repositories/activity_repository.dart
+++ lib/repositories/activity_repository.dart
@@ -2,6 +2,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 
+//
 class ActivityRepository {
   final uid = FirebaseAuth.instance.currentUser?.uid;
   final CollectionReference _activty =
